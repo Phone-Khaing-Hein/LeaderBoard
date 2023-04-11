9f686e0f97888773596862202b61faaafccec597
PhoneKhaingHein <pkh2662003@gmail.com>
2023-04-12 00:14:44 +0630

    add styles

--- src/modules/form.js
+++ src/modules/form.js
@@ -2,10 +2,10 @@ const form = () => {
   const board = document.getElementById('board');
   const form = `
         <form id="scoreForm" method="post">
-            <h3>Add your scores</h3>
-            <input type="text" placeholder="your name" name="name"> <br/>
-            <input type="number" placeholder="your scores" name="score"> <br/>
-            <button>Submit</button>
+            <h3 class="mb-3">Add your scores</h3>
+            <input type="text" placeholder="your name" name="name" class="form-control mb-3">
+            <input type="number" placeholder="your scores" name="score" class="form-control mb-3">
+            <button class="btn btn-secondary shadow float-end">Submit</button>
         </form>
   `;
 
